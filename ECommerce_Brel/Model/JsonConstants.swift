82e625ddbd27e03ed1b1d01933786e5c170a142a
Ehor Brel <ehorbrel@MacBook-Pro-Ehor.local>
2018-01-19 12:04:04 +0300

    Initial commit + add Constants

---
+++ ECommerce_Brel/Model/JsonConstants.swift
@@ -0,0 +1,35 @@
+//
+//  JsonConstants.swift
+//  ECommerce_Brel
+//
+//  Created by Ehor Brel on 19.01.18.
+//  Copyright © 2018 Ehor Brel. All rights reserved.
+//
+
+import Foundation
+
+struct Constants{
+    struct Heroku {
+        static let baseAPIURL = "https://ecommerceserv.herokuapp.com/"
+        static let pictureFolder = "images/"
+    }
+    struct HerokuGet {
+        static let get_items = "Items"
+    }
+    struct HerokuPost {
+        static let add_request = "request"
+    }
+}
+
+struct Items: Codable{
+    let arrayOfItems : Array<Item>
+}
+
+struct Item: Codable {
+    let id: Int
+    let itemName: String
+    let price: Double
+    let pictUrl: String
+}
+
+
